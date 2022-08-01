c742c55193a5fc7e099308d255a72ff0544f1d4f
gabistam <gabistam@gmail.com>
2022-08-02 01:59:22 +0200

    Add file Header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import Home from '../pages/Home';
 import Survey from '../pages/Survey'
 import { Routes, Route} from 'react-router-dom';
+import Header from './Header/Header';
 export default function App() {
   return (
     <div>
+      <Header />
+
       <Routes>
-        <Route path="/" element={<Home />} />;
+        <Route exact path="/" element={<Home />} />;
         <Route path="/survey" element={<Survey />} />;
       </Routes>
     </div>
